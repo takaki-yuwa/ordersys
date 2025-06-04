c900cec16fb6cbb3202dfd3054ea45597d6bf8de
佐藤健吾 <@AC2025-80-005>
2025-06-04 16:23:35 +0900

    注文履歴画面の作成中
    ・商品名を表示出来るように修正した
    →これからSQL文を修正する予定

--- src/dao/OrderDetailsDAO.java
+++ src/dao/OrderDetailsDAO.java
@@ -11,29 +11,44 @@ import servlet.order_details_list;
 
 public class OrderDetailsDAO {
 
-	public List<order_details_list> findOrderHistory(int search_table_number, int search_order_price) {
-		List<order_details_list> orderDetailsList = new ArrayList<>();
+    public List<order_details_list> findOrderHistory(int search_table_number, int search_order_price) {
+        List<order_details_list> orderDetailsList = new ArrayList<>();
 
-		try (Connection con = DBUtil.getConnection();
-				Statement st = con.createStatement();
-				ResultSet rs = st.executeQuery(
-						"SELECT order_details_id, order_id, product_quantity, order_price, table_number FROM order_details where table_number = "
-								+ search_table_number + " AND order_price = " + search_order_price)) {
+        try (Connection con = DBUtil.getConnection();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT order_details_id, order_id, product_quantity, order_price, table_number, accounting_flag FROM order_details where table_number = " + search_table_number + " AND order_price = " + search_order_price))
+            {
 
-			while (rs.next()) {
-				int order_details_id = rs.getInt("order_details_id");
-				int order_id = rs.getInt("order_id");
-				int product_quantity = rs.getInt("product_quantity");
-				int order_price = rs.getInt("order_price");
-				int table_number = rs.getInt("table_number");
-				orderDetailsList.add(new order_details_list(order_details_id, order_id, product_quantity, order_price,
-						table_number));
-			}
+            while (rs.next()) {
+            	int order_details_id = rs.getInt("order_details_id");
+                int order_id = rs.getInt("order_id");
+                int product_quantity = rs.getInt("product_quantity");
+                int order_price = rs.getInt("order_price");
+                int table_number = rs.getInt("table_number");
+                int accounting_flag = rs.getInt("accounting_flag");
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+                // 商品詳細テーブル取得
+                ResultSet rs_product_details = st.executeQuery("SELECT product_id FROM product_details where order_id = " + order_id);
+                if (rs_product_details.next()) {
+                    int product_id = rs_product_details.getInt("product_id");
 
-		return orderDetailsList;
-	}
+                    // 商品テーブル取得
+                    ResultSet rs_products = st.executeQuery("SELECT product_name, product_price FROM product where product_id = " + product_id);
+                    if (rs_products.next()) {
+                        String product_name = rs_products.getString("product_name");
+                        int product_price = rs_products.getInt("product_price");
+
+                        orderDetailsList.add(new order_details_list(order_details_id, order_id, product_quantity, order_price, table_number, accounting_flag,
+                                product_name, product_price));
+                    }
+                }
+            }
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return orderDetailsList;
+    }
 }
--- src/servlet/order_details_list.java
+++ src/servlet/order_details_list.java
@@ -1,38 +1,87 @@
 package servlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class order_details_list {
-	private int order_details_id;
-	private int order_id;
-	private int product_quantity;
-	private int order_price;
-	private int table_number;
+	// 注文詳細
+    private int order_details_id;
+    private int order_id;
+    private int product_quantity;
+    private int order_price;
+    private int table_number;
+    private int accounting_flag;
+    // 商品詳細
+    private String product_name;
+    private int product_price;
+    private int product_id;
+    // トッピング
+    private List<Integer> topping_id;
+    private List<Integer> topping_quantity;
+    private List<String> topping_name;
+    private List<Integer> topping_price;
 
-	public order_details_list(int order_details_id, int order_id, int product_quantity, int order_price,
-			int table_number) {
-		this.order_details_id = order_details_id;
-		this.order_id = order_id;
-		this.product_quantity = product_quantity;
-		this.order_price = order_price;
-		this.table_number = table_number;
-	}
+    public order_details_list(int order_details_id, int order_id, int product_quantity, int order_price, int table_number, int accounting_flag,
+    		String name, Integer price) {
+    	this.order_details_id = order_details_id;
+    	this.order_id = order_id;
+        this.product_quantity = product_quantity;
+        this.order_price = order_price;
+        this.table_number = table_number;
+        this.accounting_flag = accounting_flag;
+    	this.product_name = name;
+        this.product_price = price;
+    }
+    public void topping_list(List<String> name, List<Integer> price, List<Integer> topping_quantity) {
+    	this.topping_name = new ArrayList<>(name);
+        this.topping_price = new ArrayList<>(price);
+        this.topping_quantity = new ArrayList<>(topping_quantity);
+    }
+    public int getorder_details_id() {
+        return order_details_id;
+    }
+    
+    public int getorder_id() {
+        return order_id;
+    }
 
-	public int getorder_details_id() {
-		return order_details_id;
-	}
+    public int getproduct_quantity() {
+        return product_quantity;
+    }
+    
+    public int getorder_price() {
+        return order_price;
+    }
 
-	public int getorder_id() {
-		return order_id;
-	}
+    public int gettable_number() {
+        return table_number;
+    }
 
-	public int getproduct_quantity() {
-		return product_quantity;
-	}
+    public String getProduct_name() {
+        return product_name;
+    }
 
-	public int getorder_price() {
-		return order_price;
-	}
+    public Integer getProduct_price() {
+        return product_price;
+    }
 
-	public int gettable_number() {
-		return table_number;
-	}
+    public Integer getProduct_id() {
+        return product_id;
+    }
+
+    public List<String> getTopping_name() {
+        return topping_name;
+    }
+
+    public List<Integer> getTopping_price() {
+        return topping_price;
+    }
+
+    public List<Integer> getTopping_id() {
+        return topping_id;
+    }
+
+    public List<Integer> getTopping_quantity() {
+        return topping_quantity;
+    }
 }
